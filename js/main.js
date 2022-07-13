0212442ae4f5c183d40785e737291a20fa47e1f1
AlessioMaria5 <alessiomaria5@gmail.com>
2022-07-13 16:03:12 +0200

    da fixare numero cella

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
 let campo = document.getElementById('campo');
+let cella;
 
 for (i=0 ; i<100 ; i++) {
 
     let cella = document.createElement('div');
     cella.classList.add('cellaBase');
     campo.append(cella);
+    cella.addEventListener('click', 
+    function() {
 
+    cella.classList.toggle('clicked');
+    alert('la tua cella è la numero' + cella[is]);
+
+})
 }
+
+
 
