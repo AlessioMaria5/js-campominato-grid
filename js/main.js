ce597311a9ca8382d9fa61166ef9d8fd4825f26d
AlessioMaria5 <alessiomaria5@gmail.com>
2022-07-13 15:56:32 +0200

    100 celle

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let campo = document.getElementById('campo');
+
+for (i=0 ; i<100 ; i++) {
+
+    let cella = document.createElement('div');
+    cella.classList.add('cellaBase');
+    campo.append(cella);
+
+}
+
