9814062c84f46a847bbed29ca40934359c868bdc
AlessioMaria5 <alessiomaria5@gmail.com>
2022-07-13 19:30:06 +0200

    bug genera

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
 let campo = document.getElementById('campo');
 let cella;
+let fineCiclo = true;
 let bottoneStart = document.getElementById('genera');
-
-bottoneStart.addEventListener('click', 
-function(){
-
+    bottoneStart.addEventListener('click', 
+    function(){
+        
+        campo.innerHTML = '';
+        
     for (let i= 1 ;i<=100; i++) {
-
+        
         let cella = document.createElement('div');
         cella.classList.add('cellaBase');
         campo.append(cella);
@@ -14,9 +16,10 @@ function(){
         function() {
         cella.classList.toggle('clicked');
         console.log(i);
+           
     })
-    
     }
+
 })
 
 
