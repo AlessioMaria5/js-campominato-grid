97fc1d12723a0de9c36da499f141c177f0645131
AlessioMaria5 <alessiomaria5@gmail.com>
2022-07-13 18:42:26 +0200

    fatto pulsante genera

--- js/main.js
+++ js/main.js
@@ -1,21 +1,25 @@
 let campo = document.getElementById('campo');
 let cella;
+let bottoneStart = document.getElementById('genera');
 
+bottoneStart.addEventListener('click', 
+function(){
 
-for (let i= 1 ;i<=100; i++) {
+    for (let i= 1 ;i<=100; i++) {
 
+        let cella = document.createElement('div');
+        cella.classList.add('cellaBase');
+        campo.append(cella);
+        cella.addEventListener('click', 
+        function() {
+        cella.classList.toggle('clicked');
+        console.log(i);
+    })
     
-
-    let cella = document.createElement('div');
-    cella.classList.add('cellaBase');
-    campo.append(cella);
-    cella.addEventListener('click', 
-    function() {
-    cella.classList.toggle('clicked');
-    console.log(i);
+    }
 })
 
-}
+
 
 
 
