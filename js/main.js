9ff11823587d24d0addc44cc322623c25844e336
AlessioMaria5 <alessiomaria5@gmail.com>
2022-07-14 09:22:41 +0200

    da fixare bug stampa punteggio

--- js/main.js
+++ js/main.js
@@ -2,12 +2,13 @@ let campo = document.getElementById('campo');
 let livello = document.getElementById('diff');
 
 
+
 // PUNTEGGIO
 
 let punteggio = document.getElementById('punteggio');
 let numeroPunteggio = document.createElement('span');
 punteggio.append(numeroPunteggio);
-let punteggioAttuale = 0;
+let punteggioAttuale = '';
 
 
 // PUNTEGGIO
@@ -59,36 +60,61 @@ let bottoneStart = document.getElementById('genera');
             }
         }
 
-        else if(livello.value == 'Medio'){
-
-            for (let i= 1 ;i<=81; i++) {  
-                let cella = document.createElement('div');
-                cella.classList.add('cellaBase');
-                cella.style.width = 'calc(100% / 9)'
-                cella.style.height = 'calc(100% / 9)'
-                campo.append(cella);
-                cella.addEventListener('click', 
-                function() {
-                cella.classList.toggle('clicked');
-                console.log(i); 
-                if(cella.classList.contains('clicked')){
-                    punteggioAttuale++
-                    console.log(punteggioAttuale);
-                }            
-            })
-            }
-        }
+        medium();
+        
 
         //PUNTEGGIO
         
-        numeroPunteggio.innerHTML = "il tuo punteggio è " +punteggioAttuale;
+       
 })
 
 
 
 
 
+numeroPunteggio.innerHTML = "il tuo punteggio è " +punteggioAttuale;
 
+
+
+function incermentoPunteggio(x,y) {
+
+    if(x.classList.contains('clicked')){
+        y++
+        console.log(y);
+    }  
+    
+    return y
+}
+
+
+function medium() {
+
+    if(livello.value == 'Medio'){
+
+        
+
+        for (let i= 1 ;i<=81; i++) { 
+            
+            punteggioAttuale = '';
+
+            let cella = document.createElement('div');
+            cella.classList.add('cellaBase');
+            cella.style.width = 'calc(100% / 9)'
+            cella.style.height = 'calc(100% / 9)'
+            campo.append(cella);
+            cella.addEventListener('click', 
+            function () {
+            cella.classList.toggle('clicked');
+            console.log(i); 
+            if(cella.classList.contains('clicked')){
+                punteggioAttuale++
+                numeroPunteggio.append(punteggioAttuale);
+            }   
+            
+        })
+        }
+    }
+}
 
 
 
